9ee61f912c8901b058d2c7dbef8cdc93b5567fb8
Shuyi Ma <sma@sbri.org>
2017-01-24 17:52:42 -0800

    added scripts needed to create symbolic links for TBRUsnp folder

--- TBRUmd5check.sh
+++ TBRUmd5check.sh
@@ -6,6 +6,7 @@ md5sum $datafile >> md5check.txt
 done
 
 
-cat TBRUfiles1.txt | tr -d '\r' | while read LINE; do ln -s /nearline/sherman/ExpressionData/RNAseq/TBRURNA1-28225197/FASTQfiles/$filename ~/TBRUrnaseqInfo/forGEO/$LINE;done
+cat TBRUfiles1.txt | tr -d '\r' | while read LINE; do ln -s /nearline/sherman/ExpressionData/RNAseq/TBRURNA1-28225197/FASTQfiles/$LINE ~/TBRUrnaseqInfo/forGEO/$LINE;done
 
+cat TBRU1names.txt | while read LINE; do ln -s /nearline/sherman/ExpressionData/RNAseq/TBRU\ RNAseq/TBRURNA1-28225197/results/align/$LINE.genomic.bam ./results/align;done
 
